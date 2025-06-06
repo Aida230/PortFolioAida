c6e98a7076612408b8217af3812783014d2cea72
Aida Fuentes <afdlf2306@gmail.com>
2025-06-06 21:14:58 +0200

    MobileMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import { useState } from 'react'
 import { LoadingScreen } from './components/LoadingScreen'
-import { Navbar } from './components/sections/Navbar'
+import { Navbar } from './components/Navbar'
+import { MobileMenu } from './components/MobileMenu'
 import "./index.css"
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -11,7 +12,7 @@ function App() {
     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} 
     <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <MovileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
     
     </>
