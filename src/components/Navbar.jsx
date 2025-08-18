68ce9c5a17d52951b0707d998e2b4870daf79981
Aida Fuentes <afdlf2306@gmail.com>
2025-08-18 12:24:35 +0200

    modificacon del nombre

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
-            aida<span className="text-pink-800">.bluedev</span>
+            aida<span className="text-pink-800">.Fuentes</span>
           </a>
           <div
             className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
